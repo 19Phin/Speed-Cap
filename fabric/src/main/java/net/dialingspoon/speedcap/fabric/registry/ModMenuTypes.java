db55efe7aab26fea07533662efc830b6c6a1d846
19Phin <19phin@gmail.com>
2024-10-02 19:37:52 -0700

    1.0.3 - 1.20.4, fixed neoforge lang and possible screen registry issues

--- fabric/src/main/java/net/dialingspoon/speedcap/fabric/registry/ModMenuTypes.java
+++ fabric/src/main/java/net/dialingspoon/speedcap/fabric/registry/ModMenuTypes.java
@@ -2,6 +2,8 @@ package net.dialingspoon.speedcap.fabric.registry;
 
 import net.dialingspoon.speedcap.SpeedCap;
 import net.dialingspoon.speedcap.gui.SpeedCapMenu;
+import net.dialingspoon.speedcap.gui.SpeedCapScreen;
+import net.minecraft.client.gui.screens.MenuScreens;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
@@ -10,5 +12,7 @@ import net.minecraft.world.inventory.MenuType;
 
 public class ModMenuTypes {
     public static final MenuType<SpeedCapMenu> SPEEDCAP = Registry.register(BuiltInRegistries.MENU, ResourceLocation.tryBuild(SpeedCap.MOD_ID, "speed_cap"), new MenuType<>(SpeedCapMenu::new, FeatureFlags.VANILLA_SET));
-    public static void register() {}
+    public static void register() {
+        MenuScreens.register(SPEEDCAP, SpeedCapScreen::new);
+    }
 }
