9810fb46d52343f4b8ff1971c60877a7e8b2ee9e
19Phin <19phin@gmail.com>
2024-11-16 11:02:58 -0700

    tooltip

--- common/src/main/java/net/dialingspoon/speedcap/mixin/EntityMixin.java
+++ common/src/main/java/net/dialingspoon/speedcap/mixin/EntityMixin.java
@@ -11,7 +11,6 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
